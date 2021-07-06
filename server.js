bd41017911cd15d1d49737a6e05f89c363c0e2f1
Harry <barcia.harry@gmail.com>
2021-07-06 10:17:04 +0200

    creation du serveur avec fastify

---
+++ server.js
@@ -0,0 +1,34 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return "HELLO RLD"
+})
+const avengers= ["Captain America", "Thor"]
+fastify.get('/heroes', () => {
+  return avengers
+})
+console.table(avengers)
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(4000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
+
+const student ="Siham"
+const age=18
+
+const data={
+  student,
+  age,
+}
+
+fastify.get('/siham',()=>{
+  return data
+})
