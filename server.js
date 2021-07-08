1ba65dda6982226a27daa1a1956a269c41f68644
Harry Barcia <barcia.harry@gmail.com>
2021-07-08 15:11:44 +0200

    ajout token

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
 const { ObjectId } = require("mongodb");
+const argon2 = require("argon2");
+const createError = require("http-errors"); // https://www.npmjs.com/package/http-errors
+const jwt = require("fastify-jwt");
 
 //connexion à la bande de données.
 fastify.register(require("fastify-mongodb"), {
@@ -9,6 +12,10 @@ fastify.register(require("fastify-mongodb"), {
   forceClose: true,
 
   url: "mongodb://localhost:27017/superheroes",
+});
+
+fastify.register(require("fastify-jwt"), {
+  secret: "monsupersecretamoi",
 });
 
 // Declare a route
@@ -80,24 +87,26 @@ fastify.post("/heroes", async (request, reply) => {
   return result.ops[0];
 });
 
-
-
 fastify.delete("/heroes/bio/:heroesId", async (request, reply) => {
   const collection = fastify.mongo.db.collection("heroes");
   const { heroesId } = request.params;
-const result= await collection.findOneAndDelete({
-  _id:new ObjectId(heroesId)
-})
-return result
+  const result = await collection.findOneAndDelete({
+    _id: new ObjectId(heroesId),
   });
+  return result;
+});
 
-  // PUT Patch
-  fastify.patch("/heroes/:id", async (request, reply) => {
-    const collection=fastify.mongo.db.collection("heroes")
-    const {id} =request.params
-    const result=await collection.findOneAndUpdate({_id:new ObjectId(id)},{$set:request.body},{returnDocument:'after'})
-return result
-    })
+// PUT Patch
+fastify.patch("/heroes/:id", async (request, reply) => {
+  const collection = fastify.mongo.db.collection("heroes");
+  const { id } = request.params;
+  const result = await collection.findOneAndUpdate(
+    { _id: new ObjectId(id) },
+    { $set: request.body },
+    { returnDocument: "after" }
+  );
+  return result;
+});
 
 // Exercice
 
@@ -117,7 +126,7 @@ return result
   return "HELLO RLD";
 });
  */
-
+/* 
 fastify.post("/users", async (request, reply) => {
   console.log(request.body);
   const collection = fastify.mongo.db.collection("users");
@@ -134,45 +143,118 @@ fastify.get('/users', async () => {
   const result=await collection.find({}).toArray()
   return result
 })
+ */
+//password
 
+fastify.post("/users", async (request, reply) => {
+  try {
+    const collection = fastify.mongo.db.collection("users");
+    const { email, password, role } = request.body;
+
+    // On récupère l'adresse email dans la request, puis on va chercher dans la bdd si cette derniere existe
+    // Si elle existe, on genere une erreur indiquant que l'email existe deja
+    // Si non, on ajoute l'utilisateur à notre bdd
+
+    const userExist = await collection.findOne({ email });
+
+    if (userExist) {
+      // ⛔️ STOP
+      return createError(409, "Cet email est déjà pris");
+      // return createError.Conflict(err.message)
+    }
+
+    if (password.length < 3) {
+      // throw new Error("Mot de passe trop court - au moins 3 caractères")
+      return createError.NotAcceptable(
+        "Mot de passe trop court - au moins 3 caractères"
+      );
+    }
+
+    // const password = request.body.password
+    const hash = await argon2.hash(password);
+    const newUser = {
+      email: request.body.email,
+      password: hash,
+      role: request.body.role,
+    };
+    const result = await collection.insertOne(newUser);
+    // return result.ops[0]
+    reply.code(201).send(result.ops[0]);
+  } catch (err) {
+    console.error(err);
+    // reply.code(409).send({
+    // 	statusCode: 409,
+    // 	error: true,
+    // 	message: err.message
+    // })
+    return createError(500, err.message);
+  }
+});
 
 // Une route qui me permette de récupérer un utilisateur par son id
 
 //obtiens le héros ayant un id 69
-fastify.get('/users/:id', async (request,reply) => {
+fastify.get("/users/:id", async (request, reply) => {
+  //   const heroesId=request.params.heroesId
 
-//   const heroesId=request.params.heroesId
- 
-const collection=fastify.mongo.db.collection("users")
-const {id} =request.params
-const result=await collection.findOne({
-  _id:new ObjectId(id)//transforme ma chaine de caractère. C'est une "classe", instance d'un objet.
-})
-return result.email
-})
+  const collection = fastify.mongo.db.collection("users");
+  const { id } = request.params;
+  //const id =request.params.id
+  const result = await collection.findOne({
+    _id: new ObjectId(id), //transforme ma chaine de caractère. C'est une "classe", instance d'un objet.
+  });
+  return result.email;
+});
+
+fastify.post("/login", async (request, reply) => {
+  // je récupère l'email et le mot de passe entré dans request
+  // je cherche si l'utilisateur posssède cet email
+  //s'il existe: on vérifie que les passwords correspondent
+  //sinon, on génère une erreur
+  const { email, password } = request.body
+  const collection = fastify.mongo.db.collection("users");
+  const userExists = await collection.findOne({ email: email });
+
+  if (!userExists) {
+    return createError(400, "Email et/ou mot de passe incorrect");
+  }
+
+  console.log(userExists.password);
+  const match = await argon2.verify(userExists.password, password);
+  if (!match) {
+    return createError(400, "Email et/ou mot de passe incorrect");
+  }
+  // je sais que l'email et le mdp sont corrects, j'envoie un token au client permettant l'authentification
+
+    const token = fastify.jwt.sign({id:userExists._id,role:userExists.role  })
+
+  
+  return  {token};
+});
 
 // Une route qui me permette de mettre à jour un utilisateur par son id
+// PUT Patch
+fastify.patch("/users/:id", async (request, reply) => {
+  const collection = fastify.mongo.db.collection("users");
+  const { id } = request.params;
+  const result = await collection.findOneAndUpdate(
+    { _id: new ObjectId(id) },
+    { $set: request.body },
+    { returnDocument: "after" }
+  );
+  return result;
+});
 
+// Une route qui me permette de supprimer un utilisateur par son id
 
-  // PUT Patch
-  fastify.patch("/users/:id", async (request, reply) => {
-    const collection=fastify.mongo.db.collection("users")
-    const {id} =request.params
-    const result=await collection.findOneAndUpdate({_id:new ObjectId(id)},{$set:request.body},{returnDocument:'after'})
-return result
-    })
-
-    // Une route qui me permette de supprimer un utilisateur par son id
-
-    fastify.delete("/users/:Id", async (request, reply) => {
-      const collection = fastify.mongo.db.collection("users");
-      const { Id } = request.params;
-    const result= await collection.findOneAndDelete({
-      _id:new ObjectId(Id)
-    })
-    return result
-      });
-    
+fastify.delete("/users/:Id", async (request, reply) => {
+  const collection = fastify.mongo.db.collection("users");
+  const { Id } = request.params;
+  const result = await collection.findOneAndDelete({
+    _id: new ObjectId(Id),
+  });
+  return result;
+});
 
 // FIN EXERCICE//
 
